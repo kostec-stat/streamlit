e52daa3cdcb3321f539307496df79971d39d3703
KOSTEC <3344200+kostec-stat@users.noreply.github.com>
2025-04-29 05:35:52 +0900

    Update streamlitdashboard.py

--- streamlitdashboard.py
+++ streamlitdashboard.py
@@ -19,7 +19,7 @@ def local_css(file_name):
     st.markdown(f"<style>{css_content}</style>", unsafe_allow_html=True)
 
 # assets/css 폴더의 custom.css를 읽어오기
-local_css("assets/css/custom.css")
+local_css("assets/css/main.css")
 # --- 3. 데이터 불러오기
 def load_keywords(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
