43030303221be14133c89199a61e00bf684da076
KOSTEC <3344200+kostec-stat@users.noreply.github.com>
2025-05-18 20:15:32 +0900

    Update streamlitdashboard.py

--- streamlitdashboard.py
+++ streamlitdashboard.py
@@ -42,7 +42,7 @@ if st.sidebar.button("🛰 주간 동향 수집 시작"):
                 input_date = input_date[0]
 
             # 다시 확인: 날짜 형식인지
-            if isinstance(input_date, (date, datetime)):
+            if isinstance(input_date, (date)):
                 current_date = input_date.strftime("%Y%m%d")
             else:
                 st.error("❌ 날짜 형식이 올바르지 않습니다.")
